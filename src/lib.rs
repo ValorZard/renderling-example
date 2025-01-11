bb04c235d221813740b60d340fa85c4aa3b1f5dc
Srayan Jana <valorzard.kiko@outlook.com>
2025-01-11 11:16:43 -0800

    commit example

---
+++ src/lib.rs
@@ -0,0 +1,521 @@
+//! Runs through all the gltf sample models to test and show-off renderling's
+//! gltf capabilities.
+use std::{
+    collections::{HashMap, HashSet},
+    sync::{Arc, Mutex},
+};
+
+use renderling::{
+    atlas::AtlasImage,
+    bvol::{Aabb, BoundingSphere},
+    camera::Camera,
+    math::{Mat4, UVec2, Vec2, Vec3, Vec4},
+    pbr::light::{DirectionalLight, Light},
+    skybox::Skybox,
+    slab::{GpuArray, Hybrid},
+    stage::{Animator, GltfDocument, Renderlet, Stage, Vertex},
+    Context,
+};
+use renderling_ui::{FontArc, Section, Text, Ui, UiPath, UiText};
+
+pub mod camera;
+use camera::{
+    CameraControl, CameraController, TurntableCameraController, WasdMouseCameraController,
+};
+
+pub mod time;
+use time::FPSCounter;
+
+pub mod utils;
+
+const FONT_BYTES: &[u8] =
+    include_bytes!("../../../fonts/Recursive Mn Lnr St Med Nerd Font Complete.ttf");
+
+const DARK_BLUE_BG_COLOR: Vec4 = Vec4::new(
+    0x30 as f32 / 255.0,
+    0x35 as f32 / 255.0,
+    0x42 as f32 / 255.0,
+    1.0,
+);
+
+pub enum SupportedFileType {
+    Gltf,
+    Hdr,
+}
+
+pub fn is_file_supported(file: impl AsRef<std::path::Path>) -> Option<SupportedFileType> {
+    let ext = file.as_ref().extension()?;
+    Some(match ext.to_str()? {
+        "hdr" => SupportedFileType::Hdr,
+        _ => SupportedFileType::Gltf,
+    })
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+lazy_static::lazy_static! {
+    static ref START: std::time::Instant = std::time::Instant::now();
+}
+
+fn now() -> f64 {
+    #[cfg(target_arch = "wasm32")]
+    {
+        let doc = web_sys::window().unwrap();
+        let perf = doc.performance().unwrap();
+        perf.now()
+    }
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        let now = std::time::Instant::now();
+        let duration = now.duration_since(*START);
+        duration.as_secs_f64()
+    }
+}
+
+struct AppUi {
+    ui: Ui,
+    fps_text: UiText,
+    fps_counter: FPSCounter,
+    fps_background: UiPath,
+    last_fps_display: f64,
+}
+
+impl AppUi {
+    fn make_fps_widget(fps_counter: &FPSCounter, ui: &Ui) -> (UiText, UiPath) {
+        let translation = Vec2::new(2.0, 2.0);
+        let text = format!("{}fps", fps_counter.current_fps_string());
+        let fps_text = ui
+            .new_text()
+            .with_color(Vec3::ZERO.extend(1.0))
+            .with_section(Section::new().add_text(Text::new(&text).with_scale(32.0)))
+            .build();
+        fps_text.transform.set_translation(translation);
+        let background = ui
+            .new_path()
+            .with_fill_color(Vec4::ONE)
+            .with_rectangle(fps_text.bounds.0, fps_text.bounds.1)
+            .fill();
+        background.transform.set_translation(translation);
+        background.transform.set_z(-0.9);
+        (fps_text, background)
+    }
+
+    fn tick(&mut self) {
+        self.fps_counter.next_frame();
+        let now = now();
+        if now - self.last_fps_display >= 1.0 {
+            let (fps_text, background) = Self::make_fps_widget(&self.fps_counter, &self.ui);
+            self.fps_text = fps_text;
+            self.fps_background = background;
+            self.last_fps_display = now;
+        }
+    }
+}
+
+struct DefaultModel {
+    vertices: GpuArray<Vertex>,
+    renderlet: Hybrid<Renderlet>,
+}
+
+pub enum Model {
+    Gltf(GltfDocument),
+    Default(DefaultModel),
+    None,
+}
+
+pub struct App {
+    last_frame_instant: f64,
+    skybox_image_bytes: Option<Vec<u8>>,
+    loads: Arc<Mutex<HashMap<std::path::PathBuf, Vec<u8>>>>,
+    pub stage: Stage,
+    camera: Hybrid<Camera>,
+    _light: Option<(Hybrid<DirectionalLight>, Hybrid<Light>)>,
+    model: Model,
+    animators: Option<Vec<Animator>>,
+    animations_conflict: bool,
+    pub camera_controller: Box<dyn CameraController + 'static>,
+    ui: AppUi,
+}
+
+impl App {
+    pub fn new(ctx: &Context, camera_control: CameraControl) -> Self {
+        let stage = ctx
+            .new_stage()
+            .with_background_color(DARK_BLUE_BG_COLOR)
+            .with_bloom_mix_strength(0.5)
+            .with_bloom_filter_radius(4.0)
+            .with_msaa_sample_count(4)
+            .with_debug_overlay(true);
+        let camera = stage.new_value(Camera::default());
+        // let sunlight = stage.new_value(DirectionalLight {
+        //     direction: Vec3::NEG_Y,
+        //     color: hex_to_vec4(0xFDFBD3FF),
+        //     intensity: 10.0,
+        // });
+        // let light = stage.new_value(Light::from(sunlight.id()));
+        // stage.set_lights([light.id()]);
+
+        stage
+            .set_atlas_size(wgpu::Extent3d {
+                width: 2048,
+                height: 2048,
+                depth_or_array_layers: 32,
+            })
+            .unwrap();
+
+        let ui = Ui::new(ctx).with_background_color(Vec4::ZERO);
+        let _ = ui.add_font(FontArc::try_from_slice(FONT_BYTES).unwrap());
+        let fps_counter = FPSCounter::default();
+        let (fps_text, fps_background) = AppUi::make_fps_widget(&fps_counter, &ui);
+
+        Self {
+            ui: AppUi {
+                ui,
+                fps_text,
+                fps_counter,
+                fps_background,
+                last_fps_display: now(),
+            },
+            stage,
+            camera,
+            _light: None,
+
+            model: Model::None,
+            animators: None,
+            animations_conflict: false,
+
+            skybox_image_bytes: None,
+            loads: Arc::new(Mutex::new(HashMap::default())),
+            last_frame_instant: now(),
+
+            camera_controller: match camera_control {
+                CameraControl::Turntable => Box::new(TurntableCameraController::default()),
+                CameraControl::WasdMouse => Box::new(WasdMouseCameraController::default()),
+            },
+        }
+    }
+
+    pub fn tick(&mut self) {
+        self.camera_controller.tick();
+        self.tick_loads();
+        self.update_view();
+        self.animate();
+        self.ui.tick();
+    }
+
+    pub fn render(&self, ctx: &Context) {
+        let frame = ctx.get_next_frame().unwrap();
+        self.stage.render(&frame.view());
+        self.ui.ui.render(&frame.view());
+        frame.present();
+    }
+
+    pub fn update_view(&mut self) {
+        self.camera_controller
+            .update_camera(self.stage.get_size(), &self.camera);
+    }
+
+    fn load_hdr_skybox(&mut self, bytes: Vec<u8>) {
+        let img = AtlasImage::from_hdr_bytes(&bytes).unwrap();
+        let (device, queue) = self.stage.get_device_and_queue_owned();
+        let skybox = Skybox::new(&device, &queue, img, self.camera.id());
+        self.skybox_image_bytes = Some(bytes);
+        self.stage.set_skybox(skybox);
+    }
+
+    pub fn load_default_model(&mut self) {
+        let mut min = Vec3::splat(f32::INFINITY);
+        let mut max = Vec3::splat(f32::NEG_INFINITY);
+        let vertices = self
+            .stage
+            .new_array(renderling::math::unit_cube().into_iter().map(|(p, n)| {
+                let p = p * 2.0;
+                min = min.min(p);
+                max = max.max(p);
+                Vertex::default()
+                    .with_position(p)
+                    .with_normal(n)
+                    .with_color(Vec4::new(1.0, 0.0, 0.0, 1.0))
+            }));
+        log::info!("default model bounds: {min} {max}");
+        let bounds = BoundingSphere::from((min, max));
+        self.camera_controller.reset(Aabb::new(min, max));
+        self.camera_controller
+            .update_camera(self.stage.get_size(), &self.camera);
+        self.last_frame_instant = now();
+        let renderlet = self.stage.new_value(Renderlet {
+            vertices_array: vertices.array(),
+            camera_id: self.camera.id(),
+            bounds,
+            ..Default::default()
+        });
+        self.model = Model::Default(DefaultModel {
+            vertices: vertices.into_gpu_only(),
+            renderlet,
+        });
+    }
+
+    fn load_gltf_model(&mut self, path: impl AsRef<std::path::Path>, bytes: &[u8]) {
+        log::info!("loading gltf");
+        self.camera_controller
+            .reset(Aabb::new(Vec3::NEG_ONE, Vec3::ONE));
+        self.stage.clear_images().unwrap();
+        self.model = Model::None;
+        log::debug!("ticking stage to reclaim buffers");
+        self.stage.tick();
+        let doc = match self
+            .stage
+            .load_gltf_document_from_bytes(bytes, self.camera.id())
+        {
+            Err(e) => {
+                log::error!("gltf loading error: {e}");
+                if cfg!(not(target_arch = "wasm32")) {
+                    log::info!("attempting to load by filesystem");
+                    match self
+                        .stage
+                        .load_gltf_document_from_path(path, self.camera.id())
+                    {
+                        Ok(doc) => doc,
+                        Err(e) => {
+                            log::error!("gltf loading error: {e}");
+                            return;
+                        }
+                    }
+                } else {
+                    return;
+                }
+            }
+            Ok(doc) => doc,
+        };
+
+        // self.entities = builder.entities.clone();
+
+        // find the bounding box of the model so we can display it correctly
+        let mut min = Vec3::splat(f32::INFINITY);
+        let mut max = Vec3::splat(f32::NEG_INFINITY);
+
+        let scene = doc.default_scene.unwrap_or(0);
+        log::info!("Displaying scene {scene}");
+        fn get_children(doc: &GltfDocument, n: usize) -> Vec<usize> {
+            let mut children = vec![];
+            if let Some(parent) = doc.nodes.get(n) {
+                children.extend(parent.children.iter().copied());
+                let descendants = parent
+                    .children
+                    .iter()
+                    .copied()
+                    .flat_map(|n| get_children(doc, n));
+                children.extend(descendants);
+            }
+            children
+        }
+
+        let nodes = doc.nodes_in_scene(scene).flat_map(|n| {
+            let mut all_nodes = vec![n];
+            for child_index in get_children(&doc, n.index) {
+                if let Some(child_node) = doc.nodes.get(child_index) {
+                    all_nodes.push(child_node);
+                }
+            }
+            all_nodes
+        });
+        log::trace!("  nodes:");
+        for node in nodes {
+            let tfrm = Mat4::from(node.global_transform());
+            if let Some(mesh_index) = node.mesh {
+                // UNWRAP: safe because we know the node exists
+                for primitive in doc.meshes.get(mesh_index).unwrap().primitives.iter() {
+                    let bbmin = tfrm.transform_point3(primitive.bounding_box.0);
+                    let bbmax = tfrm.transform_point3(primitive.bounding_box.1);
+                    min = min.min(bbmin);
+                    max = max.max(bbmax);
+                }
+            }
+        }
+
+        log::trace!("Bounding box: {min} {max}");
+        let bounding_box = Aabb::new(min, max);
+        self.camera_controller.reset(bounding_box);
+        self.camera_controller
+            .update_camera(self.stage.get_size(), &self.camera);
+
+        self.last_frame_instant = now();
+
+        if doc.animations.is_empty() {
+            log::trace!("  animations: none");
+        } else {
+            log::trace!("  animations:");
+        }
+        let mut animated_nodes = HashSet::default();
+        let mut has_conflicting_animations = false;
+        self.animators = Some(
+            doc.animations
+                .iter()
+                .enumerate()
+                .map(|(i, a)| {
+                    let target_nodes = a.target_node_indices().collect::<HashSet<_>>();
+                    has_conflicting_animations =
+                        has_conflicting_animations || !animated_nodes.is_disjoint(&target_nodes);
+                    animated_nodes.extend(target_nodes);
+
+                    log::trace!("    {i} {:?} {}s", a.name, a.length_in_seconds());
+                    // for (t, tween) in a.tweens.iter().enumerate() {
+                    //     log::trace!(
+                    //         "      tween {t} targets node {} {}",
+                    //         tween.target_node_index,
+                    //         tween.properties.description()
+                    //     );
+                    // }
+                    Animator::new(doc.nodes.iter(), a.clone())
+                })
+                .collect(),
+        );
+        if has_conflicting_animations {
+            log::trace!("  and some animations conflict");
+        }
+        self.animations_conflict = has_conflicting_animations;
+        self.model = Model::Gltf(doc);
+    }
+
+    pub fn tick_loads(&mut self) {
+        let loaded = std::mem::take(&mut *self.loads.lock().unwrap());
+        for (path, bytes) in loaded.into_iter() {
+            log::info!("loaded {}bytes from {}", bytes.len(), path.display());
+            match is_file_supported(&path) {
+                Some(SupportedFileType::Gltf) => self.load_gltf_model(path, &bytes),
+                Some(SupportedFileType::Hdr) => self.load_hdr_skybox(bytes),
+                None => {}
+            }
+        }
+    }
+
+    /// Queues a load operation.
+    pub fn load(&mut self, path: &str) {
+        let path = std::path::PathBuf::from(path);
+        let loads = self.loads.clone();
+
+        #[cfg(target_arch = "wasm32")]
+        {
+            wasm_bindgen_futures::spawn_local(async move {
+                let path_str = format!("{}", path.display());
+                let bytes = loading_bytes::load(&path_str).await.unwrap();
+                let mut loads = loads.lock().unwrap();
+                loads.insert(path, bytes);
+                log::debug!("loaded {path_str}");
+            });
+        }
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            let _ = std::thread::spawn(move || {
+                let bytes = std::fs::read(&path)
+                    .unwrap_or_else(|e| panic!("could not load '{}': {e}", path.display()));
+                let mut loads = loads.lock().unwrap();
+                loads.insert(path, bytes);
+            });
+        }
+    }
+
+    pub fn set_size(&mut self, size: UVec2) {
+        self.stage.set_size(size);
+    }
+
+    pub fn animate(&mut self) {
+        let now = now();
+        let dt_seconds = now - self.last_frame_instant;
+        self.last_frame_instant = now;
+        self.camera_controller.tick();
+        if let Some(animators) = self.animators.as_mut() {
+            if self.animations_conflict {
+                if let Some(animator) = animators.first_mut() {
+                    animator.progress(dt_seconds as f32).unwrap();
+                }
+            } else {
+                for animator in animators.iter_mut() {
+                    animator.progress(dt_seconds as f32).unwrap();
+                }
+            }
+        }
+    }
+}
+
+// /// Sets up the demo for a given model
+// pub async fn demo(
+//     ctx: &Context,
+//     model: Option<impl AsRef<str>>,
+//     skybox: Option<impl AsRef<str>>,
+// ) -> impl FnMut(&mut Context, Option<&winit::event::WindowEvent>) {
+//     let mut app = App::new(ctx).await;
+//     if let Some(file) = model {
+//         app.load(file.as_ref());
+//     }
+//     if let Some(file) = skybox {
+//         app.load(file.as_ref());
+//     }
+//     let mut event_state = renderling_gpui::EventState::default();
+//     move |r, ev: Option<&winit::event::WindowEvent>| {
+//         if let Some(ev) = ev {
+//             match ev {
+//                 winit::event::WindowEvent::MouseWheel { delta, .. } => {
+//                     let delta = match delta {
+//                         winit::event::MouseScrollDelta::LineDelta(_, up) =>
+// *up,                         winit::event::MouseScrollDelta::PixelDelta(pos)
+// => pos.y as f32,                     };
+
+//                     app.zoom(r, delta);
+//                 }
+//                 winit::event::WindowEvent::CursorMoved { position, .. } => {
+//                     app.pan(r, *position);
+//                 }
+//                 winit::event::WindowEvent::MouseInput { state, button, .. }
+// => {                     app.mouse_button(*state, *button);
+//                 }
+//                 winit::event::WindowEvent::KeyboardInput { input, .. } => {
+//                     app.key_input(r, *input);
+//                 }
+//                 winit::event::WindowEvent::Resized(size) => {
+//                     log::trace!("resizing to {size:?}");
+//                     app.resize(r, size.width, size.height);
+//                     let _ = app
+//                         .gpui
+//                         .0
+//                         .graph
+//                         .get_resource::<ScreenSize>()
+//                         .unwrap()
+//                         .unwrap();
+//                 }
+//                 winit::event::WindowEvent::DroppedFile(path) => {
+//                     log::trace!("got dropped file event: {}",
+// path.display());                     let path = format!("{}",
+// path.display());                     app.load(&path);
+//                 }
+//                 _ => {}
+//             }
+
+//             if let Some(ev) = event_state.event_from_winit(ev) {
+//                 let scene =
+// r.graph.get_resource_mut::<Scene>().unwrap().unwrap();                 let
+// channel = scene.get_debug_channel();                 let mut
+// set_debug_channel = |mode| {                     log::debug!("setting debug
+// mode to {mode:?}");                     if channel != mode {
+//                         scene.set_debug_channel(mode);
+//                     } else {
+//                         scene.set_debug_channel(DebugChannel::None);
+//                     }
+//                 };
+
+//                 match app.ui.event(ev) {
+//                     None => {}
+//                     Some(ev) => match ev {
+//                         UiEvent::ToggleDebugChannel(channel) =>
+// set_debug_channel(channel),                     },
+//                 }
+//             }
+//         } else {
+//             app.tick_loads(r);
+//             app.update_camera_view(r);
+//             app.animate(r);
+//             app.gpui.layout(&mut app.ui);
+//             app.gpui.render(&mut app.ui);
+//             r.render().unwrap();
+//         }
+//     }
+// }
---
+++ src/main.rs
@@ -0,0 +1,151 @@
+//! Main entry point for the gltf viewer.
+use std::sync::Arc;
+
+use clap::Parser;
+use example::{camera::CameraControl, App};
+use renderling::{
+    math::{UVec2, Vec2},
+    Context,
+};
+use winit::{application::ApplicationHandler, event::WindowEvent, window::WindowAttributes};
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+struct Cli {
+    /// Optional gltf model to load at startup
+    #[arg(short, long)]
+    model: Option<String>,
+
+    /// Optional HDR image to use as skybox at startup
+    #[arg(short, long)]
+    skybox: Option<String>,
+
+    /// Camera control scheme
+    #[arg(short, long, default_value = "turntable")]
+    camera_control: CameraControl,
+    // /// Optional number of repeat instances of the same model
+    // #[arg(short, long)]
+    // repeat_n: Option<u32>,
+}
+
+struct InnerApp {
+    ctx: Context,
+    app: App,
+}
+
+impl InnerApp {
+    fn tick(&mut self) {
+        self.app.tick();
+    }
+
+    fn event(&mut self, event: WindowEvent) -> bool {
+        match event {
+            winit::event::WindowEvent::DroppedFile(path) => {
+                log::trace!("got dropped file event: {}", path.display());
+                let path = format!("{}", path.display());
+                self.app.load(&path);
+            }
+
+            winit::event::WindowEvent::CloseRequested
+            | winit::event::WindowEvent::KeyboardInput {
+                event:
+                    winit::event::KeyEvent {
+                        logical_key: winit::keyboard::Key::Named(winit::keyboard::NamedKey::Escape),
+                        ..
+                    },
+                ..
+            } => return true,
+            winit::event::WindowEvent::Resized(size) => {
+                let size = UVec2::new(size.width, size.height);
+                self.ctx.set_size(size);
+                self.app.set_size(size);
+            }
+            winit::event::WindowEvent::RedrawRequested => {
+                self.ctx.get_device().poll(wgpu::Maintain::Wait);
+            }
+            e => self.app.camera_controller.window_event(e),
+        }
+        false
+    }
+}
+
+struct OuterApp {
+    cli: Cli,
+    inner: Option<InnerApp>,
+}
+
+impl ApplicationHandler for OuterApp {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        let window_size = winit::dpi::LogicalSize {
+            width: 800,
+            height: 600,
+        };
+        let attributes = WindowAttributes::default()
+            .with_inner_size(window_size)
+            .with_title("renderling gltf viewer");
+        let window = Arc::new(event_loop.create_window(attributes).unwrap());
+
+        // Set up a new renderling context
+        let ctx = Context::try_from_window(None, window.clone()).unwrap();
+        let mut app = App::new(&ctx, self.cli.camera_control);
+        if let Some(file) = self.cli.model.as_ref() {
+            log::info!("loading model '{file}'");
+            app.load(file.as_ref());
+        } else {
+            log::info!("loading default model");
+            app.load_default_model();
+        }
+        if let Some(file) = self.cli.skybox.as_ref() {
+            log::info!("loading skybox '{file}'");
+            app.load(file.as_ref());
+        }
+        self.inner = Some(InnerApp { ctx, app });
+    }
+
+    fn about_to_wait(&mut self, _event_loop: &winit::event_loop::ActiveEventLoop) {
+        if let Some(inner) = self.inner.as_mut() {
+            inner.tick();
+            inner.app.render(&inner.ctx);
+        }
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        _window_id: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        if let Some(inner) = self.inner.as_mut() {
+            if inner.event(event) {
+                event_loop.exit();
+            }
+        }
+    }
+
+    fn device_event(
+        &mut self,
+        _event_loop: &winit::event_loop::ActiveEventLoop,
+        _device_id: winit::event::DeviceId,
+        event: winit::event::DeviceEvent,
+    ) {
+        if let Some(inner) = self.inner.as_mut() {
+            if let winit::event::DeviceEvent::MouseMotion { delta } = event {
+                inner
+                    .app
+                    .camera_controller
+                    .mouse_motion(Vec2::new(delta.0 as f32, delta.1 as f32))
+            }
+        }
+    }
+}
+
+fn main() {
+    let cli = Cli::parse();
+    env_logger::builder().init();
+    log::info!("starting up with options: {cli:#?}");
+
+    let event_loop = winit::event_loop::EventLoop::new().unwrap();
+    event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
+    let mut outer_app = OuterApp { cli, inner: None };
+    event_loop.run_app(&mut outer_app).unwrap();
+}
